5411a63b91b770979f05e9a6de2bef8fefcbb4eb
1802448 <1802448@POR7400329.exchange.ad.afpanet>
2019-03-06 16:53:17 +0100

    glozo

--- FrontRestNosecurity/src/main/java/fr/afpa/formation/Application.java
+++ FrontRestNosecurity/src/main/java/fr/afpa/formation/Application.java
@@ -1,5 +1,6 @@
 package fr.afpa.formation;
 
+//to jissi
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
